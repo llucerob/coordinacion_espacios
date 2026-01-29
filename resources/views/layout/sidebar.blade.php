6fc333d865aa340ae727b1bd875563d62e9bb0c7
NicolasGuerra-sys <bardock76@gmail.com>
2026-01-29 14:16:43 -0300

    dashboard

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,70 +1,117 @@
-<!-- Page Sidebar Start-->
 <div class="sidebar-wrapper" data-sidebar-layout="stroke-svg">
     <div>
-        <div class="logo-wrapper text-center"><a href="{{route('dashboard')}}"><img class="img-fluid for-light"
-                    src="{{ asset('assets/images/logo/logo_serviciosayf.png') }}" width="80px" alt=""></a>
-            <div class="back-btn"><i class="fa-solid fa-angle-left"></i></div>
-            <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid">
-                </i></div>
+        <div class="logo-wrapper">
+            <a href="#">
+                <img class="img-fluid for-light" src="{{ asset('assets/images/logo/logom.png') }}" alt="">
+                <img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt="">
+            </a>
+            <div class="back-btn"><i class="fa fa-angle-left"></i></div>
+            <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"> </i></div>
         </div>
-        <div class="logo-icon-wrapper"><a href="#"><img class="img-fluid"
-                    src="{{ asset('assets/images/logo/logo-icon.png') }}" alt=""></a></div>
+        <div class="logo-icon-wrapper">
+            <a href="#"><img class="img-fluid" src="{{ asset('assets/images/logo/logo-icon.png') }}" alt=""></a>
+        </div>
         <nav class="sidebar-main">
             <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
             <div id="sidebar-menu">
                 <ul class="sidebar-links" id="simple-bar">
-                    <li class="back-btn"><a href="#"><img class="img-fluid"
-                                src="{{ asset('assets/images/logo/logo-icon.png') }}" alt=""></a>
-                        <div class="mobile-back text-end"><span>Back</span><i class="fa-solid fa-angle-right ps-2"
-                                aria-hidden="true"></i></div>
+                    <li class="back-btn">
+                        <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
                     </li>
-                    <li class="pin-title sidebar-main-title">
-                        <div>
-                            <h6>Favoritos</h6>
-                        </div>
+                    <li class="sidebar-list pt-4 mt-2">
+                        <a class="sidebar-link sidebar-title mt-3" href="{{route('dashboard')}}">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-home') }}"></use>
+                            </svg>
+                            <span>Todas las Reservas</span>
+                        </a>
                     </li>
-                    @role(['admin'])  
                     <li class="sidebar-main-title">
                         <div>
-                            <h6>Valorizaciones</h6>
+                            <h6>Actividades</h6>
                         </div>
                     </li>
-                    <!--<li class="sidebar-list"><i class="fa-solid fa-thumbtack"> </i><a
-                        class="sidebar-link sidebar-title" href="javascript:void(0)"><svg class="stroke-icon">
-                            <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-project') }}"></use>
-                        </svg><svg class="fill-icon">
-                            <use href="{{ asset('assets/svg/icon-sprite.svg#fill-project') }}"></use>
-                        </svg><span>Usuarios</span></a>
-                    <ul class="sidebar-submenu">
-                        
-                        <li><a href="#">Listar Usuarios</a></li>
-                        <li><a href="#">Crear Usuario</a></li>
-                    </ul>
-                    </li>!-->
-                    <li class="sidebar-list"><i class="fa-solid fa-thumbtack"> </i><a
-                        class="sidebar-link sidebar-title" href="javascript:void(0)"><svg class="stroke-icon">
-                            <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-project') }}"></use>
-                        </svg><svg class="fill-icon">
-                            <use href="{{ asset('assets/svg/icon-sprite.svg#fill-project') }}"></use>
-                        </svg><span>Valorizaciones</span></a>
-                    <ul class="sidebar-submenu">
-                        
-                        <li><a href="#">Trabajos Realizados</a></li>
-                        <li><a href="#">Historial Valorizaciones</a></li>
-                        <li><a href="#">Valorizar Bodega</a></li>
-                        
-                    </ul>
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-to-do') }}"></use>
+                            </svg>
+                            <span>Nueva Actividad</span>
+                        </a>
                     </li>
-                    @endrole
-               
-                    
-                     
-                   
-                    
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-calendar') }}"></use>
+                            </svg>
+                            <span>Agendar Actividad</span>
+                        </a>
+                        <ul class="sidebar-submenu" style="display: block;">
+                            <li><a href="#">Agendamiento</a></li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-main-title">
+                        <div>
+                            <h6>RECINTOS</h6>
+                        </div>
+                    </li>
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-table') }}"></use>
+                            </svg>
+                            <span>Recintos</span>
+                        </a>
+                        <ul class="sidebar-submenu" style="display: block;">
+                            <li><a href="#">Crear Recinto</a></li>
+                            <li><a href="#">Listar Recintos</a></li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-project') }}"></use>
+                            </svg>
+                            <span>Materiales</span>
+                        </a>
+                        <ul class="sidebar-submenu" style="display: block;">
+                            <li><a href="#">Crear Materiales</a></li>
+                            <li><a href="#">Listar Materiales</a></li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-main-title">
+                        <div>
+                            <h6>ENCARGADOS</h6>
+                        </div>
+                    </li>
+                   <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-user') }}"></use>
+                            </svg>
+                            <span>Encargados</span>
+                        </a>
+                        <ul class="sidebar-submenu" style="display: block;">
+                            <li><a href="#">Crear Encargado</a></li>
+                            <li><a href="#">Listar Encargados</a></li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-list">
+                        <a class="sidebar-link sidebar-title" href="#">
+                            <svg class="stroke-icon">
+                                <use href="{{ asset('assets/svg/icon-sprite.svg#stroke-table') }}"></use>
+                            </svg>
+                            <span>Departamentos</span>
+                        </a>
+                        <ul class="sidebar-submenu" style="display: block;">
+                            <li><a href="#">Crear Departamento</a></li>
+                            <li><a href="#">Listar Departamentos</a></li>
+                        </ul>
+                    </li>
+
                 </ul>
             </div>
             <div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
         </nav>
     </div>
 </div>
-<!-- Page Sidebar Ends-->
